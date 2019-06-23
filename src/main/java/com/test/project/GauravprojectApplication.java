8a02158fe97b48eae86905181b9fb8fa7e30a186
vikas paul <yogivikaspaul@gmail.com>
2019-06-23 12:21:55 +0530

    First Commit

---
+++ src/main/java/com/test/project/GauravprojectApplication.java
@@ -0,0 +1,13 @@
+package com.test.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GauravprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GauravprojectApplication.class, args);
+	}
+
+}
